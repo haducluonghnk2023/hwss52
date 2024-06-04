5884b126b395cfb3293ea698a1ef3cd447bf35e0
haducluonghnk2023 <haducluongdz@gmail.com>
2024-06-04 08:50:42 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Navbar from "./components/navbar/Navbar";
+import Element from "./components/hw3/Service";
+import Contact1 from "./components/hw3/Contact";
+import About1 from "./components/hw3/About";
+import Index from "./components/hw3/Index";
+import Service from "./components/hw3/Service";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/About" element={<About1 />} />
+        <Route path="/Contact" element={<Contact1 />} />
+        <Route path="/Element" element={<Element />} />
+        <Route path="/Index" element={<Index />} />
+        <Route path="/Sevice" element={<Service />} />
       </Routes>
     </div>
   );
